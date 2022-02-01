0a11ecfe6b750cd84c0cc61844951b0461506f8b
marcelo-sl <mar.sousa2061@gmail.com>
2022-02-01 20:00:09 -0300

    Changing findById tests

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.repositories;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,61 +14,61 @@ import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long existingId;
 	private long nonExistingId;
 	private long countTotalProducts;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
 	}
-	
+
 	@Test
 	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
-		
+
 		Product product = Factory.createProduct();
 		product.setId(null);
-		
+
 		product = repository.save(product);
-		
+
 		Assertions.assertNotNull(product.getId());
 		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 	}
-	
+
 	@Test
-	public void deleteShouldDeleteObjectWhenIdExists() {		
+	public void deleteShouldDeleteObjectWhenIdExists() {
 		repository.deleteById(existingId);
-		
-		Optional<Product> result = repository.findById(existingId);		
-		Assertions.assertFalse(result.isPresent());		
+
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertFalse(result.isPresent());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
 		});
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnNonEmptyOptionalProductObjectWhenIdExists() {
-		
-		Product result = repository.findById(existingId).get();
-		
-		Assertions.assertNotNull(result);
+
+		Optional<Product> result = repository.findById(existingId);
+
+		Assertions.assertTrue(result.isPresent());
 	}
-	
+
 	@Test
 	public void findByIdShouldReturnEmptyOptionalProductObjectWhenIdNotExists() {
-		
-		Assertions.assertThrows(NoSuchElementException.class, () -> {
-			repository.findById(nonExistingId).get();
-		});		
+
+		Optional<Product> result = repository.findById(nonExistingId);
+
+		Assertions.assertTrue(result.isEmpty());
 	}
 }
